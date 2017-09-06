c9746e6b185f3750ee5a2e773e51996705624ce9
luislocon <luis.locon@gmail.com>
2017-09-05 22:58:05 -0600

    Adding the style starting files and create components

---
+++ client/reduxstagram.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { render } from 'react-dom';
+
+import Main from './components/Main';
+// import css
+import css from './styles/style.styl';
+
+render(<Main />, document.getElementById('root'));
