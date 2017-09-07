77ec7f27cbe80635c9e208f9746c675dbaf420ab
luislocon <luis.locon@gmail.com>
2017-09-06 22:32:29 -0600

    Displaying comment and adding new comments

--- client/components/Single.js
+++ client/components/Single.js
@@ -7,14 +7,15 @@ import Comments from './Comments';
 const Single = React.createClass({
   render() {
     // Index of the post
-    const i = this.props.posts.findIndex(post => post.code === this.props.params.postId);
-    
+    const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
     const post = this.props.posts[i];
+    const postComments = this.props.comments[this.props.params.postId] || [];
+
     // get us the post
     return (
       <div className="single-photo">
         <Photo i={i} post={post} {...this.props} />
-        <Comments />
+        <Comments postComments={postComments} />
       </div>
     )
   }
