00d5861bbeadf7bda1a175e560cf07b5fcd9b2db
luislocon <luis.locon@gmail.com>
2017-09-06 22:10:11 -0600

    Updating state with reducers

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -3,7 +3,19 @@
 // 2. copy of current state
 
 function posts(state= [], action) {
-  console.log(state, action);
+  switch (action.type) {
+    case 'INCREMENT_LIKES':
+      //RETURN THE UPDATE STATE
+      // console.log('Incrementing likes');
+      const i = action.index;
+      return [
+        ...state.slice(0, i), //before the one we are updating
+        {...state[i], likes: state[i].likes + 1},
+        ...state.slice(i + 1), //after the one we are updating
+      ]
+    default:
+      return state;
+  }
   return state;
 }
 
