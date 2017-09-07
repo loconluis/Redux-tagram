958edb23a8ff7b86552ae7a1590e50e8911085f1
luislocon <luis.locon@gmail.com>
2017-09-06 21:56:32 -0600

    Showing data to the frontend with the component PHOTO

--- client/components/Photogrid.js
+++ client/components/Photogrid.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import Photo from './Photo';
 
 
 const PhotoGrid = React.createClass({
   render() {
     return (
       <div className="photo-grid">
-        I'm photogrid
+        {this.props.posts.map((post, index) => 
+        <Photo
+          {...this.props}
+          key={index}
+          i={index}
+          post={post}
+        />)}
       </div>
     )
   }
